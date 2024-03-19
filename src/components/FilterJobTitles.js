0776784b95ac475d9ca71e692399fc2d7f005a5f
Sarfaraz Shaik <sarfarazshaik2807@gmail.com>
2024-03-19 10:43:49 +0530

    Filter with job titles

--- src/components/FilterJobTitles.js
+++ src/components/FilterJobTitles.js
@@ -1,14 +1,15 @@
- import React from 'react'
+import React from 'react'
 
-function FilterJobTitles({jobTitle}) {
-  console.log(jobTitle)
+function FilterJobTitles({jobTitle, onTitleSelect}) {
   return (
     <header className="flex bg-white p-3 gap-2 shadow-xl sticky top-0 z-10">
       <button className="py-2 px-4  border min-w-16 min-h-10 rounded-full hover:bg-slate-100 hover:shadow overflow-hidden text-ellipsis whitespace-normal">
         All
       </button>
       {jobTitle.map((title) => (
-        <button key={title} className="py-2 px-4 border min-w-16 min-h-10 rounded-full  hover:bg-slate-100 hover:shadow overflow-hidden text-ellipsis whitespace-normal">
+        <button key={title} 
+                className="py-2 px-4 border min-w-16 min-h-10 rounded-full  hover:bg-slate-100 hover:shadow overflow-hidden text-ellipsis whitespace-normal"
+                onClick={() => onTitleSelect(title)}>
           {title}
         </button>
       ))}
